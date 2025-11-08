b502594a85630d7725222fd4c7977c5feed2b569
Jaroenchart <jrcsjang@gmail.com>
2025-11-08 17:06:42 +0700

    Color and animation

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -14,7 +14,18 @@ export default function App() {
 
       {/* Decorative gradient background */}
       <div aria-hidden className="pointer-events-none fixed inset-0 -z-10">
-        <div className="absolute -top-24 left-1/2 h-[40rem] w-[40rem] -translate-x-1/2 rounded-full bg-brand-500/20 blur-3xl"></div>
+        <div
+          className="absolute -top-24 left-1/2 h-[50rem] w-[80rem] -translate-x-1/2
+                  rounded-full bg-amber-400/50 dark:bg-sky-500/50 blur-3xl
+                  will-change-[opacity,transform]
+                  animate-[twinkle-soft_8s_ease-in-out_infinite]"
+        />
+        <div
+          className="absolute -top-20 left-[30%] h-[34rem] w-[42rem]
+                  rounded-full bg-amber-400/30 dark:bg-sky-500/30 blur-3xl
+                  opacity-70 will-change-[opacity,transform]
+                  animate-[twinkle-soft_4s_ease-in-out_infinite_2.1s]"
+        />
       </div>
 
       <main>
